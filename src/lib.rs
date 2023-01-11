881d0ed43d213f217ea3140c5b03c2e492247328
rileyseaburg <riley@rileyseaburg.com>
2023-01-10 21:35:39 -0500

    Stable CLI - new project creates working Rocket server.

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,10 @@
 
 use clap::builder::Str;
 use clap::{arg, Arg, ArgAction, Args, Command, Parser, Subcommand};
-use std::io::Write;
+use std::{
+    fs::{create_dir, File},
+    io::Write,
+};
 
 type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
@@ -87,9 +90,15 @@ pub struct Project {
     reset_password_page_html: String,
     forgot_password_page_html: String,
     dashboard_page_html: String,
+    user_controller_directory: String,
     user_controller: String,
+    user_controller_module: String,
+    user_model_directory: String,
     user_model: String,
+    user_model_module: String,
+    user_migration_directory: String,
     user_migration: String,
+    user_migration_module: String,
     user_seeder: String,
     user_test: String,
     user_routes: String,
@@ -166,9 +175,18 @@ impl Project {
         let forgot_password_page_html =
             format!("{}/templates/pages/forgot_password.html.tera", name);
         let dashboard_page_html = format!("{}/templates/pages/dashboard.html.tera", name);
-        let user_controller = format!("{}/controllers/user.rs", name);
-        let user_model = format!("{}/models/user.rs", name);
-        let user_migration = format!("{}/migrations/00000000000000_create_users_table.rs", name);
+        let user_controller_directory = format!("{}/controllers/user", name);
+        let user_controller = format!("{}/user.rs", user_controller_directory);
+        let user_controller_module = format!("{}/mod.rs", user_controller_directory);
+        let user_model_directory = format!("{}/models/user", name);
+        let user_model = format!("{}/user.rs", user_model_directory);
+        let user_model_module = format!("{}/mod.rs", user_model_directory);
+        let user_migration_directory = format!("{}/migrations/user", name);
+        let user_migration = format!(
+            "{}/00000000000000_create_users_table.rs",
+            user_migration_directory
+        );
+        let user_migration_module = format!("{}/mod.rs", user_migration_directory);
         let user_seeder = format!("{}/seeders/00000000000000_create_users_table.rs", name);
         let user_test = format!("{}/tests/user.rs", name);
         let user_routes = format!("{}/routes/user.rs", name);
@@ -223,9 +241,15 @@ impl Project {
             reset_password_page_html,
             forgot_password_page_html,
             dashboard_page_html,
+            user_controller_directory,
             user_controller,
+            user_controller_module,
+            user_model_directory,
             user_model,
+            user_model_module,
+            user_migration_directory,
             user_migration,
+            user_migration_module,
             user_seeder,
             user_test,
             user_routes,
@@ -233,78 +257,71 @@ impl Project {
     }
 
     pub fn create_directories(&self) -> Result<()> {
-        std::fs::create_dir(&self.name).expect("Failed to create directory");
-        std::fs::create_dir(&self.src_dir).expect("Failed to create src directory");
-        std::fs::create_dir(&self.templates).expect("Failed to create templates directory");
-        std::fs::create_dir(&self.static_dir).expect("Failed to create static directory");
-        std::fs::create_dir(&self.template_components)
+        create_dir(&self.name).expect("Failed to create directory");
+        create_dir(&self.src_dir).expect("Failed to create src directory");
+        create_dir(&self.templates).expect("Failed to create templates directory");
+        create_dir(&self.static_dir).expect("Failed to create static directory");
+        create_dir(&self.template_components)
             .expect("Failed to create template components directory");
-        std::fs::create_dir(&self.template_layouts)
-            .expect("Failed to create template layouts directory");
-        std::fs::create_dir(&self.template_pages)
-            .expect("Failed to create template pages directory");
-        std::fs::create_dir(&self.static_css).expect("Failed to create static css directory");
-        std::fs::create_dir(&self.static_js).expect("Failed to create static js directory");
-        std::fs::create_dir(&self.static_images).expect("Failed to create static images directory");
-        std::fs::create_dir(&self.config).expect("Failed to create config directory");
-        std::fs::create_dir(&self.config_env)
-            .expect("Failed to create config environments directory");
-        std::fs::create_dir(&self.config_database)
-            .expect("Failed to create config database directory");
-        std::fs::create_dir(&self.routes).expect("Failed to create routes directory");
-        std::fs::create_dir(&self.controllers).expect("Failed to create controllers directory");
-        std::fs::create_dir(&self.models).expect("Failed to create models directory");
-        std::fs::create_dir(&self.migrations).expect("Failed to create migrations directory");
-        std::fs::create_dir(&self.seeders).expect("Failed to create seeders directory");
-        std::fs::create_dir(&self.tests).expect("Failed to create tests directory");
-        std::fs::create_dir(&self.config_initializers)
+        create_dir(&self.template_layouts).expect("Failed to create template layouts directory");
+        create_dir(&self.template_pages).expect("Failed to create template pages directory");
+        create_dir(&self.static_css).expect("Failed to create static css directory");
+        create_dir(&self.static_js).expect("Failed to create static js directory");
+        create_dir(&self.static_images).expect("Failed to create static images directory");
+        create_dir(&self.config).expect("Failed to create config directory");
+        create_dir(&self.config_env).expect("Failed to create config environments directory");
+        create_dir(&self.config_database).expect("Failed to create config database directory");
+        create_dir(&self.routes).expect("Failed to create routes directory");
+        create_dir(&self.controllers).expect("Failed to create controllers directory");
+        create_dir(&self.models).expect("Failed to create models directory");
+        create_dir(&self.migrations).expect("Failed to create migrations directory");
+        create_dir(&self.seeders).expect("Failed to create seeders directory");
+        create_dir(&self.tests).expect("Failed to create tests directory");
+        create_dir(&self.config_initializers)
             .expect("Failed to create config initializers directory");
-        std::fs::create_dir(&self.user_controller)
+        create_dir(&self.user_controller_directory)
             .expect("Failed to create user controller directory");
-        std::fs::create_dir(&self.user_model).expect("Failed to create user model directory");
-        std::fs::create_dir(&self.user_migration)
+        create_dir(&self.user_model_directory).expect("Failed to create user model directory");
+        create_dir(&self.user_migration_directory)
             .expect("Failed to create user migration directory");
-        std::fs::create_dir(&self.user_seeder).expect("Failed to create user seeder directory");
-        std::fs::create_dir(&self.user_test).expect("Failed to create user test directory");
-        std::fs::create_dir(&self.user_routes).expect("Failed to create user routes directory");
         Ok(())
     }
 
     pub fn create_files(&self) -> Result<()> {
-        std::fs::File::create(&self.cargo_toml).expect("Failed to create Cargo.toml");
-        std::fs::File::create(&self.main_rs).expect("Failed to create main.rs");
-        std::fs::File::create(&self.package_json).expect("Failed to create package.json");
-        std::fs::File::create(&self.readme).expect("Failed to create README.md");
-        std::fs::File::create(&self.gitignore).expect("Failed to create .gitignore");
-        std::fs::File::create(&self.index_html).expect("Failed to create index.html.tera");
-        std::fs::File::create(&self.styles_css).expect("Failed to create styles.css");
-        std::fs::File::create(&self.not_found_html).expect("Failed to create 404.html.tera");
-        std::fs::File::create(&self.server_error_html).expect("Failed to create 500.html.tera");
-        std::fs::File::create(&self.favicon_ico).expect("Failed to create favicon.ico");
-        std::fs::File::create(&self.robots_txt).expect("Failed to create robots.txt");
-        std::fs::File::create(&self.login_page_html).expect("Failed to create login.html.tera");
-        std::fs::File::create(&self.signup_page_html).expect("Failed to create signup.html.tera");
-        std::fs::File::create(&self.reset_password_page_html)
+        File::create(&self.cargo_toml).expect("Failed to create Cargo.toml");
+        File::create(&self.main_rs).expect("Failed to create main.rs");
+        File::create(&self.package_json).expect("Failed to create package.json");
+        File::create(&self.readme).expect("Failed to create README.md");
+        File::create(&self.gitignore).expect("Failed to create .gitignore");
+        File::create(&self.index_html).expect("Failed to create index.html.tera");
+        File::create(&self.styles_css).expect("Failed to create styles.css");
+        File::create(&self.not_found_html).expect("Failed to create 404.html.tera");
+        File::create(&self.server_error_html).expect("Failed to create 500.html.tera");
+        File::create(&self.favicon_ico).expect("Failed to create favicon.ico");
+        File::create(&self.robots_txt).expect("Failed to create robots.txt");
+        File::create(&self.login_page_html).expect("Failed to create login.html.tera");
+        File::create(&self.signup_page_html).expect("Failed to create signup.html.tera");
+        File::create(&self.reset_password_page_html)
             .expect("Failed to create reset_password.html.tera");
-        std::fs::File::create(&self.forgot_password_page_html)
+        File::create(&self.forgot_password_page_html)
             .expect("Failed to create forgot_password.html.tera");
-        std::fs::File::create(&self.dashboard_page_html)
-            .expect("Failed to create dashboard.html.tera");
-        std::fs::File::create(&self.user_controller).expect("Failed to create user.rs");
-        std::fs::File::create(&self.user_model).expect("Failed to create user.rs");
-        std::fs::File::create(&self.user_migration).expect("Failed to create user.rs");
-        std::fs::File::create(&self.user_seeder).expect("Failed to create user.rs");
-        std::fs::File::create(&self.user_test).expect("Failed to create user.rs");
-        std::fs::File::create(&self.user_routes).expect("Failed to create user.rs");
-        std::fs::File::create(&self.config_dev_env).expect("Failed to create dev.env");
-        std::fs::File::create(&self.config_prod_env).expect("Failed to create prod.env");
-        std::fs::File::create(&self.config_test_env).expect("Failed to create test.env");
-        std::fs::File::create(&self.config_dev_db).expect("Failed to create dev.db");
-        std::fs::File::create(&self.config_prod_db).expect("Failed to create prod.db");
-        std::fs::File::create(&self.config_test_db).expect("Failed to create test.db");
-        std::fs::File::create(&self.config_initializers_db).expect("Failed to create db.rs");
-        std::fs::File::create(&self.config_initializers_routes)
-            .expect("Failed to create routes.rs");
+        File::create(&self.dashboard_page_html).expect("Failed to create dashboard.html.tera");
+        File::create(&self.user_controller).expect("Failed to create user.rs");
+        File::create(&self.user_controller_module).expect("Failed to create user.rs");
+        File::create(&self.user_model).expect("Failed to create user.rs");
+        File::create(&self.user_migration).expect("Failed to create user.rs");
+        File::create(&self.user_migration_module).expect("Failed to create user.rs");
+        File::create(&self.user_seeder).expect("Failed to create user.rs");
+        File::create(&self.user_test).expect("Failed to create user.rs");
+        File::create(&self.user_routes).expect("Failed to create user.rs");
+        File::create(&self.config_dev_env).expect("Failed to create dev.env");
+        File::create(&self.config_prod_env).expect("Failed to create prod.env");
+        File::create(&self.config_test_env).expect("Failed to create test.env");
+        File::create(&self.config_dev_db).expect("Failed to create dev.db");
+        File::create(&self.config_prod_db).expect("Failed to create prod.db");
+        File::create(&self.config_test_db).expect("Failed to create test.db");
+        File::create(&self.config_initializers_db).expect("Failed to create db.rs");
+        File::create(&self.config_initializers_routes).expect("Failed to create routes.rs");
         Ok(())
     }
 
@@ -361,7 +378,7 @@ rocket = \"0.5.0-rc.1\"",
         let mut file = std::fs::OpenOptions::new()
             .write(true)
             .append(true)
-            .open(format!("{}/package.json", &self.package_json))
+            .open(&self.package_json)
             .expect("Failed to open package.json");
 
         file.write_all(
@@ -397,7 +414,7 @@ rocket = \"0.5.0-rc.1\"",
         let mut file = std::fs::OpenOptions::new()
             .write(true)
             .append(true)
-            .open(format!("{}/README.md", &self.readme))
+            .open(&self.readme)
             .expect("Failed to open README.md");
 
         file.write_all(
@@ -436,7 +453,7 @@ This project is licensed under the MIT License - see the [LICENSE](LICENSE) file
         let mut file = std::fs::OpenOptions::new()
             .write(true)
             .append(true)
-            .open(format!("{}/.gitignore", &self.gitignore))
+            .open(&self.gitignore)
             .expect("Failed to open .gitignore");
 
         file.write_all(
@@ -459,7 +476,7 @@ static/styles.css
         let mut file = std::fs::OpenOptions::new()
             .write(true)
             .append(true)
-            .open(format!("{}/index.js", &self.index_js))
+            .open(&self.index_js)
             .expect("Failed to open index.js");
 
         file.write_all(
@@ -492,7 +509,7 @@ rustyroadster.greet();
         let mut file = std::fs::OpenOptions::new()
             .write(true)
             .append(true)
-            .open(format!("{}/dev.env", &self.config_dev_env))
+            .open(&self.config_dev_env)
             .expect("Failed to open dev.env");
         file.write_all(
             b"ROCKET_ENV=dev
@@ -517,7 +534,7 @@ rustyroadster.greet();
         let mut file = std::fs::OpenOptions::new()
             .write(true)
             .append(true)
-            .open(format!("{}/prod.env", &self.config_prod_env))
+            .open(&self.config_prod_env)
             .expect("Failed to open prod.env");
         file.write_all(
             b"ROCKET_ENV=prod
