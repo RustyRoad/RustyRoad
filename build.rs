c8d8f798c5e33b87264200c49659e711b7f43ab0
rileyseaburg <riley@rileyseaburg.com>
2023-08-21 18:25:55 -0400

    solves #13

--- build.rs
+++ build.rs
@@ -1,6 +1,19 @@
+extern crate pkg_config;
+
 fn main() {
-    // Specify the library search path
-    println!("cargo:rustc-link-search=native=C:\\Users\\riley\\vcpkg\\installed\\x64-windows\\lib");
-    // Specify the library to link
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-search=native=C:\\Program Files (x86)\\vcpkg\\installed\\x64-windows\\lib");
+    } else if cfg!(target_os = "linux") {
+        // Try to use pkg-config to find libpq
+        if let Err(e) = pkg_config::probe_library("libpq") {
+            println!("Failed to find libpq with pkg-config: {}", e);
+            // If pkg-config fails, you can fallback to the hard-coded paths:
+            println!("cargo:rustc-link-search=native=/usr/lib/");
+            println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu/");
+            println!("cargo:rustc-link-search=native=/usr/local/lib/");
+            println!("cargo:rustc-link-search=native=/lib/");
+        }
+    }
+
     println!("cargo:rustc-link-lib=libpq");
 }
