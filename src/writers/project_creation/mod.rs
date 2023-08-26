d6694089708677f2fbe1b9de1d1ed415168320ec
rileyseaburg <riley@rileyseaburg.com>
2023-08-26 09:56:04 -0400

    Refactor project_creation module: rename 'pages' to 'views'
    This commit updates the project_creation module by renaming the 'pages' module to 'views' for better clarity and consistency in naming.

--- src/writers/project_creation/mod.rs
+++ src/writers/project_creation/mod.rs
@@ -3,11 +3,11 @@ pub mod controllers;
 pub mod create_file;
 pub mod models;
 pub mod new_project;
-pub mod pages;
+pub mod views;
 
 pub use config_writers::*;
 pub use controllers::*;
 pub use create_file::*;
 pub use models::*;
 pub use new_project::*;
-pub use pages::*;
+pub use views::*;
