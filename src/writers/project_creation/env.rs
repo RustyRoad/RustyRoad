319037df006284edd30dcd558cac8ade06849dcb
rileyseaburg <riley@rileyseaburg.com>
2023-08-27 12:01:22 -0400

    Set environment variable for project creation
    - Increase the length of the generated random string to 100 characters
    - Generate a random 100-character string for the SECRET_KEY variable
    - Write the contents to the project's env file
    - Return "success" if successful, otherwise return the error message if failed

--- src/writers/project_creation/env.rs
+++ src/writers/project_creation/env.rs
@@ -3,25 +3,26 @@ use eyre::Error;
 use rand::{distributions::Alphanumeric, Rng};
 use crate::Project;
 use crate::writers::write_to_file;
-pub fn set_env(project: Project) -> Result<String, Error> {
+pub fn set_env(project: &Project) -> Result<String, Error> {
      // generate a random 32 bit string that is 50 characters long
     let random_string: String = rand::thread_rng()
         .sample_iter(&Alphanumeric)
-        .take(50)
+        .take(100)
         .map(char::from)
         .collect();
 
     // create an env in root directory
     let contents = format!(
-        "SECRET_KEY={};"
+        "SECRET_KEY={};",
         random_string
     );
 
     let mut result = "success".to_string();
 
 
+
     write_to_file(&project.env, contents.as_bytes()).unwrap_or_else(|why| {
-       result = "failed".to_string();
+       result = why.to_string();
     });
 
     Ok(result)
