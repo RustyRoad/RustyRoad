77e134afef852aa65010066d72972042f3872e05
rileyseaburg <riley@rileyseaburg.com>
2023-08-30 22:48:38 -0400

    feat: Update grapesjs.rs
    - Added imports for file system operations and error handling
    - Implemented function to write to grapes model and handle errors
    - Created logic to add contents to edit_page.html and handle errors
    - Added migration logic for grapes_js table creation

--- src/features/grapesjs.rs
+++ src/features/grapesjs.rs
@@ -1,5 +1,13 @@
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::Write;
 use crate::writers::{add_new_controller_to_main_rs, write_to_controller_name_html, write_to_file, write_to_new_get_controller};
 use crate::models::grapes_js::*;
+use color_eyre::eyre::Result;
+use eyre::Error;
+use crate::database::{create_migration, find_migration_dir, MigrationDirection, run_migration};
+use crate::generators::create_file;
+
 pub struct GrapesJS();
 
 impl GrapesJS {
@@ -22,9 +30,12 @@ impl GrapesJS {
 
         write_to_controller_name_html("edit_page").expect("Couldn't write to edit_page.html.tera");
 
+        write_to_grapes_model().await.expect("Couldn't write to grapes model");
 
-        // set grapesJs
-        let Html = HtmlGrapesJs::new();
+        write_to_edit_page_html();
+
+        // run the migrations
+        run_migration("grapes_js".to_string(), MigrationDirection::Up).await.expect("Couldn't run grapes_js migration");
     }
 }
 
@@ -100,6 +111,80 @@ pub fn write_to_edit_page_html() {
     </script>
     "#.to_string();
 
+    create_file("/views/pages/edit_page.html").unwrap_or_else(|_| panic!("Error: Could not create edit_page.html"));
+
     write_to_file("/views/pages/edit_page.html", contents.as_bytes())
         .unwrap_or_else(|why| panic!("Couldn't write to edit_page.html: {}", why.to_string()));
+}
+
+
+pub async fn write_to_grapes_model() -> Result<(), Error> {
+    fs::read_to_string("rustyroad.toml")
+        .unwrap_or_else(|_| panic!("Error: This is not a RustyRoad project. Please run `rustyroad new` to create a new project."));
+    let grapes_model_file_location = "src/models/grapes_js.rs";
+
+    // create the file
+    create_file(grapes_model_file_location.clone()).unwrap_or_else(|_| panic!("Error: Could not create {}", grapes_model_file_location.clone()));
+
+    let mut grapes_model_file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(grapes_model_file_location.clone())
+        .unwrap_or_else(|_| panic!("Error: Could not open {}", grapes_model_file_location.clone()));
+
+
+
+    let grapes_model_contents = format!(r#"
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct HtmlGrapesJs {{
+        pub id: i32,
+        pub html_content: String,
+        pub created_at: DateTime<chrono::Utc>,
+        pub updated_at: DateTime<chrono::Utc>,
+        pub associated_user_id: i32,
+        pub metadata: String,
+    }}
+
+    impl HtmlGrapesJs {{
+        pub fn new() -> Self {{
+            Self {{
+                id: 0,
+                html_content: "".to_string(),
+                created_at: chrono::Utc::now(),
+                updated_at: chrono::Utc::now(),
+                associated_user_id: 0,
+                metadata: "".to_string(),
+            }}
+        }}
+    }}"#);
+
+    grapes_model_file.write_all(grapes_model_contents.as_bytes()).unwrap_or_else(|_| panic!("Error: Could not write to {}", grapes_model_file_location.clone()));
+
+
+    let grapes_js_migration_file_location = create_migration("grapes_js").await.unwrap_or_else(|_| panic!("Error: Could not create migration for grapes_js"));
+
+    // convert to a string
+    let grapes_js_migration_file_location = grapes_js_migration_file_location;
+
+    let grapes_js_migration_contents = format!(r#"
+    CREATE TABLE IF NOT EXISTS grapes_js (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        html_content TEXT NOT NULL,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        associated_user_id INTEGER NOT NULL,
+        metadata TEXT NOT NULL
+    );
+    "#);
+
+    // get the migration directory
+    let grapes_js_migration_directory = find_migration_dir("/config/database/migrations".to_string(), "grapes_js".to_string()).unwrap_or_else(|_| panic!("Error: Could not find migration directory for grapes_js"));
+
+    // write to the up.sql file
+write_to_file(format!("{}/up.sql", grapes_js_migration_directory).as_str(), grapes_js_migration_contents.as_bytes()).unwrap_or_else(|_| panic!("Error: Could not write to up.sql for grapes_js"));
+
+    // write to the down.sql file
+write_to_file(format!("{}/down.sql", grapes_js_migration_directory).as_str(), "DROP TABLE grapes_js;".as_bytes()).unwrap_or_else(|_| panic!("Error: Could not write to down.sql for grapes_js"));
+    Ok(())
 }
