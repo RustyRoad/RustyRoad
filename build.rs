20e374204cdb763221ba55d7967e1f987a94a60e
Riley Seaburg <riley@rileyseaburg.com>
2023-09-02 08:27:43 -0400

    build.rs: Add platform-specific lib path and fallbacks for finding libpq
    This commit adds platform-specific library paths to build.rs for finding libpq. On Windows, the path is set using the value of the POSTGRES_LIB_PATH environment variable. On Linux, if pkg-config fails to find libpq, the commit adds fallback paths including /usr/lib, /usr/lib/x86_64-linux-gnu, /usr/local/lib, and /lib. The commit also updates the command for running npm build in grapesjs-tailwind directory.

--- build.rs
+++ build.rs
@@ -1,24 +1,30 @@
 extern crate pkg_config;
-
+use std::env;
 use std::process::Command;
 
 fn main() {
     if cfg!(target_os = "windows") {
-        println!("cargo:rustc-link-search=native=C:\\Program Files (x86)\\vcpkg\\installed\\x64-windows\\lib");
+        let pg_path = match env::var("POSTGRES_LIB_PATH") {
+            Ok(val) => val,
+            Err(_) => panic!("Could not find environment variable POSTGRES_LIB_PATH. Make sure it's set and valid."),
+        };
+
+        println!("cargo:rustc-link-search=native={}", pg_path);
+
     } else if cfg!(target_os = "linux") {
         // Try to use pkg-config to find libpq
         if let Err(e) = pkg_config::probe_library("libpq") {
-            println!("Failed to find libpq with pkg-config: {}", e);
-            // If pkg-config fails, you can fallback to the hard-coded paths:
-            println!("cargo:rustc-link-search=native=/usr/lib/");
-            println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu/");
-            println!("cargo:rustc-link-search=native=/usr/local/lib/");
-            println!("cargo:rustc-link-search=native=/lib/");
+            println!("Failed to find libpq with pkg_config: {}", e);
+            // If pkg-config fails, try falling back to default lib search paths
+            println!("cargo:rustc-link-search=native=/usr/lib");
+            println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+            println!("cargo:rustc-link-search=native=/usr/local/lib");
+            println!("cargo:rustc-link-search=native=/lib");
         }
-
     }
 
     println!("cargo:rustc-link-lib=libpq");
+
     Command::new("cmd")
         .args(&["/C", "nvm use 18.17.0 && npm run build"])
         .current_dir("./grapesjs-tailwind")
