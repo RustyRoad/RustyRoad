384bd4a16d08cb1aa73d4c3085b14545df6e9179
Riley Seaburg <riley@rileyseaburg.com>
2023-09-08 00:39:55 -0400

    Remove environment variable print statements and unnecessary cargo link options, and switch to using the appropriate commands based on the target OS.
    Details:
    - Removed print statements for "VCPKG_ROOT" and "POSTGRES_LIB_PATH" environment variables.
    - Removed the unnecessary `cargo:rustc-link-search=native` and `cargo:rustc-link-lib` options.
    - Modified the command from `Command::new("cmd")` to `Command::new("sh")` for Linux-based OS.

--- build.rs
+++ build.rs
@@ -3,16 +3,11 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    println!("VCPKG_ROOT: {:?}", env::var("VCPKG_ROOT"));
-    println!("POSTGRES_LIB_PATH: {:?}", env::var("POSTGRES_LIB_PATH"));
     if cfg!(target_os = "windows") {
         let pg_path = match env::var("POSTGRES_LIB_PATH") {
             Ok(val) => val,
             Err(_) => panic!("Could not find environment variable POSTGRES_LIB_PATH. Make sure it's set and valid."),
         };
-
-        println!("cargo:rustc-link-search=native={}", pg_path);
-
     } else if cfg!(target_os = "linux") {
         // Try to use pkg-config to find libpq
         if let Err(e) = pkg_config::probe_library("libpq") {
@@ -24,9 +19,6 @@ fn main() {
             println!("cargo:rustc-link-search=native=/lib");
         }
     }
-
-    println!("cargo:rustc-link-lib=libpq");
-
     Command::new("cmd")
         .args(&["/C", "nvm use 18.17.1 && npm run build"])
         .current_dir("./grapesjs-tailwind")
