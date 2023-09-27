69d32e6b2f72c86dbc8411eac284d7dc89bb6658
Riley Seaburg <riley@rileyseaburg.com>
2023-09-27 18:23:14 -0400

    Add new controller writer for Actix Web
    This commit adds new functions to write controller files for Actix Web. The 'write_to_controller_name_html' function writes the contents to an HTML template file and the 'write_to_controller_name_html_with_authorized_view' function writes the contents to an HTML template file with an authenticated layout. The 'write_to_new_get_controller' function writes a new controller handler function to a Rust source file, and the 'write_to_new_post_controller' function writes a new controller handler function for HTTP POST requests to a Rust source file.

---
+++ src/writers/controllers/mod.rs
@@ -0,0 +1,13 @@
+pub mod controller_writer;
+pub mod create_controllers;
+pub mod create_read_controllers;
+pub mod create_create_controller;
+pub mod create_update_controller;
+pub mod create_delete_controller;
+
+pub use controller_writer::*;
+pub use create_controllers::*;
+pub use create_read_controllers::*;
+pub use create_create_controller::*;
+pub use create_update_controller::*;
+pub use create_delete_controller::*;
