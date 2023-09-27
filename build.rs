276dcc39c2a5142b21d90af5d4366c9323154009
Riley Seaburg <riley@rileyseaburg.com>
2023-09-27 14:08:23 -0400

    Copy grapesjs-tailwind.min.js to build directory
    - Print current working directory and source file path
    - Check if source file exists
    - Copy source file to build directory

--- build.rs
+++ build.rs
@@ -33,11 +33,26 @@ fn main() {
         .status()
         .unwrap();
 
-    // Additional logic to copy the JavaScript file
+  
+    // Print the current working directory.
+    println!("Current dir: {:?}", env::current_dir());
+    
+    // Define the source file path.
+    let source_path = Path::new("grapesjs-tailwind/dist/grapesjs-tailwind.min.js");
+    
+    // Print the absolute path of the source file.
+    println!("Source file path: {:?}", source_path.canonicalize());
+    
+    // Check whether the source file exists and print a message.
+    if source_path.exists() {
+        println!("Source file exists.");
+    } else {
+        println!("Source file does not exist.");
+    }
+    
+    // Perform the file copy operation.
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("grapesjs-tailwind.min.js");
-    fs::copy(
-        "grapesjs-tailwind/dist/grapesjs-tailwind.min.js",
-        &dest_path,
-    ).unwrap_or_else(|err| panic!("Failed to copy file: {}", err));
+    fs::copy(&source_path, &dest_path)
+        .unwrap_or_else(|err| panic!("Failed to copy file: {}", err));
 }
