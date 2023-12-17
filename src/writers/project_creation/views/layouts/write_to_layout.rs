5bf23cf54a7ed6144c2d4d9aa4c72b2b5ccd15b0
Riley Seaburg <riley@rileyseaburg.com>
2023-12-17 15:35:41 -0500

    Optimize layout template structure
    Remove outdated HTML comment and redundant block declarations. Simplify layout template for better maintainability and readability. This change makes future modifications and customizations more straightforward. The main content division now includes a 'relative' class to support UI enhancements.

--- src/writers/project_creation/views/layouts/write_to_layout.rs
+++ src/writers/project_creation/views/layouts/write_to_layout.rs
@@ -3,35 +3,28 @@ use eyre::Error;
 use crate::{Project, writers::write_to_file};
 
 pub fn write_to_layout(project: Project) -> Result<(), Error> {
-    let contents = r#"{% extends 'base.html.tera' %}
-{% block title %}{{title | default(value="Dashboard", boolean=true)}}{% endblock title %}
-{% block head %}
-{{ super() }}
-{% endblock head %}
-{% block content %}
-
-
-<!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-white">
-    <body class="h-full">
-    ```
-  -->
-{% block sidebar %}
-<!-- Sidebar content here -->
-{% endblock sidebar %}
-
-
-<main class="lg:pl-72">
-  <div id='content'>
-    {% block main_content %}
-    <!-- Main content here -->
-    {% endblock main_content %}
-  </div>
-</main>
-{% endblock content %}"#.to_string();
+    let contents = r#"
+    {% extends 'base.html.tera' %}
+    {% block title %}{{title | default(value="Dashboard", boolean=true)}}{% endblock title %}
+    {% block head %}
+    {{ super() }}
+    {% endblock head %}
+    {% block content %}
+    
+    {% block sidebar %}
+    <!-- Sidebar content here -->
+    {% endblock sidebar %}
+    
+    
+    <main class="lg:pl-72">
+      <div id='content' class='relative'>
+        {% block main_content %}
+        <!-- Main content here -->
+        {% endblock main_content %}
+      </div>
+    </main>
+    {% endblock content %}
+"#.to_string();
 
     write_to_file(&project.layout_template, contents.as_bytes()).unwrap_or_else(|why| {
         panic!(
