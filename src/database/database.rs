af0c0a1b89992119eeb286dfd6763287e40c09e2
Riley Seaburg <riley@rileyseaburg.com>
2025-05-26 14:20:05 -0500

    Bump version to 1.0.21 and enhance database module documentation
    - Updated version in Cargo.toml from 1.0.20 to 1.0.21.
    - Added detailed documentation for the Database struct and its methods, including examples for better clarity.
    - Improved the get_database_from_rustyroad_toml method to support environment-specific configuration files.
    - Refactored create_database_connection method to streamline database creation logic.
    - Introduced PoolConnection enum with documentation for different database connection pools.

--- src/database/database.rs
+++ src/database/database.rs
@@ -1,4 +1,5 @@
 use crate::database::{get_mysql_pool, get_pg_pool, get_sqlite_pool};
+use crate::writers::create_database_if_not_exists;
 use sqlx::mysql::{MySqlConnectOptions, MySqlPool};
 use sqlx::postgres::{PgConnectOptions, PgPool};
 use sqlx::sqlite::SqlitePool;
@@ -6,7 +7,6 @@ use std::error::Error;
 use std::fs;
 use std::sync::Arc;
 use toml::Value;
-use crate::writers::create_database_if_not_exists;
 
 use super::databasetype::DatabaseType;
 
@@ -27,7 +27,53 @@ pub enum DatabaseConnection {
     Sqlite(Arc<SqlitePool>),
 }
 
+/// # Name: Database
+/// ## Description
+/// Struct representing a database connection configuration.
+/// ## Fields
+/// * `name` - The name of the database.
+/// * `username` - The username for the database connection.
+/// * `password` - The password for the database connection.
+/// * `host` - The host where the database is located.
+/// * `port` - The port on which the database is running.
+/// * `database_type` - The type of the database (e.g., MySQL, PostgreSQL, SQLite).
+/// ## Example
+/// ```
+/// use rustyroad::database::Database;
+/// let db = Database::new(
+///     "my_database".to_string(),
+///    "my_user".to_string(),
+///   "my_password".to_string(),
+///   "localhost".to_string(),
+///  5432,
+/// "postgres",
+/// );
+/// ```
 impl Database {
+    /// # Name: new
+    /// ## Description
+    /// Creates a new instance of the `Database` struct.
+    /// ## Arguments
+    /// * `name` - The name of the database.
+    /// * `username` - The username for the database connection.
+    /// * `password` - The password for the database connection.
+    /// * `host` - The host where the database is located.
+    /// * `port` - The port on which the database is running.
+    /// * `database_type` - The type of the database (e.g., "postgres", "mysql", "sqlite").
+    /// ## Returns
+    /// * `Database` - A new instance of the `Database` struct.
+    /// ## Example
+    /// ```rust
+    /// use rustyroad::database::Database;
+    /// let db = Database::new(
+    ///    "my_database".to_string(),
+    ///    "my_user".to_string(),
+    ///    "my_password".to_string(),
+    ///    "localhost".to_string(),
+    ///    5432,
+    ///    "postgres",
+    /// );
+    /// ```
     pub fn new(
         name: String,
         username: String,
@@ -51,6 +97,24 @@ impl Database {
         }
     }
 
+    /// # Name: create_database_connection
+    /// ## Description
+    /// Creates a database connection based on the database type.
+    /// ## Returns
+    /// * `Result<DatabaseConnection, Box<dyn Error + Send>>` - A result containing the database connection or an error.
+    /// ## Example
+    /// ```rust
+    /// use rustyroad::database::Database;
+    /// let db = Database::new(
+    ///   "my_database".to_string(),
+    ///   "my_user".to_string(),
+    ///   "my_password".to_string(),
+    ///   "localhost".to_string(),
+    ///   5432,
+    ///   "postgres",
+    /// );
+    /// let connection = db.create_database_connection().await.unwrap();
+    /// ```
     pub async fn create_database_connection(
         &self,
     ) -> Result<DatabaseConnection, Box<dyn Error + Send>> {
@@ -74,18 +138,14 @@ impl Database {
                 Ok(DatabaseConnection::Sqlite(Arc::new(pool)))
             }
             DatabaseType::Postgres => {
-
                 let database: Database = Database::get_database_from_rustyroad_toml().unwrap();
                 let admin_database_url = format!(
                     "postgres://{}:{}@{}:{}/postgres",
-                    database.username,
-                    database.password,
-                    database.host,
-                    database.port,
+                    database.username, database.password, database.host, database.port,
                 );
-                create_database_if_not_exists(
-                    admin_database_url.as_str(), database).await.unwrap_or_else(|_| panic!("Failed to create database"));
-
+                create_database_if_not_exists(admin_database_url.as_str(), database)
+                    .await
+                    .unwrap_or_else(|_| panic!("Failed to create database"));
 
                 let options = PgConnectOptions::new()
                     .username(&self.username)
@@ -103,9 +163,25 @@ impl Database {
         }
     }
 
+    /// # Name: get_database_from_rustyroad_toml
+    /// ## Description
+    /// Reads the database configuration from the `rustyroad.toml` file based on the current environment.
+    /// ## Returns
+    /// * `Result<Database, std::io::Error>` - A result containing the `Database` struct or an error if the file could not be read.
+    /// ## Example
+    /// ```rust
+    /// use rustyroad::database::Database;
+    /// let database = Database::get_database_from_rustyroad_toml().unwrap();
+    /// ```
     pub fn get_database_from_rustyroad_toml() -> Result<Database, std::io::Error> {
-        let file = fs::read_to_string("rustyroad.toml")
-            .unwrap_or_else(|_| panic!("Error: Could not find rustyroad.toml"));
+        let environment = std::env::var("ENVIRONMENT").unwrap_or("dev".to_string());
+        let file_name = if environment == "dev" {
+            "rustyroad.toml".to_string()
+        } else {
+            format!("rustyroad.{}.toml", environment)
+        };
+        let file = fs::read_to_string(file_name)
+            .unwrap_or_else(|_| panic!("Error: Could not find the configuration file"));
         let toml: Value = toml::from_str(&file).unwrap();
         let database_table = toml["database"].as_table().unwrap();
         Ok(Database::new(
@@ -172,6 +248,21 @@ impl Database {
     }
 }
 
+/// # Name: PoolConnection
+/// ## Description
+/// Enum representing different types of database connection pools.
+/// ## Variants
+/// * `Pg` - PostgreSQL connection pool.
+/// * `MySql` - MySQL connection pool.
+/// * `Sqlite` - SQLite connection pool.
+/// ## Example
+/// ```rust
+/// use rustyroad::database::PoolConnection;
+/// let pg_pool: PoolConnection = PoolConnection::Pg(sqlx::PgPool::connect("postgres://user:password@localhost/dbname").await.unwrap());
+/// let mysql_pool: PoolConnection = PoolConnection::MySql(sqlx::MySqlPool::connect("mysql://user:password@localhost/dbname").await.unwrap());
+/// let sqlite_pool: PoolConnection = PoolConnection::Sqlite(sqlx::SqlitePool::connect("sqlite://my_database.db").await.unwrap());
+/// ```
+#[derive(Debug, Clone)]
 pub enum PoolConnection {
     Pg(sqlx::PgPool),
     MySql(sqlx::MySqlPool),
